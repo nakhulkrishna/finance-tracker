3221f57ee2327decd61386d729256362bb4eac3e
Nakhul krishna <nakhulkrishna253@gmail.com>
2026-04-08 11:32:06 +0530

    Initial Commit

---
+++ Finance Tracker/Finance_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_TrackerApp.swift
+//  Finance Tracker
+//
+//  Created by nakhul krishna on 08/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Finance_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
